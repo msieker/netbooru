fdde7f911786d876937a888b4ee8885ea765cf45
Matt Seieker <matt@siekernet.com>
2015-03-27 22:28:57 -0500

    Initial Commit

---
+++ src/Netbooru/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Logging.Console;
+
+namespace Netbooru
+{
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			Configuration = new Configuration()
+				//.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+		}
+
+		public IConfiguration Configuration { get; set; }
+
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+		{
+			loggerfactory.AddConsole();
+
+			// Add the following to the request pipeline only in development environment.
+			if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+			{
+				app.UseBrowserLink();
+				//app.UseErrorPage(ErrorPageOptions.ShowAll);
+				//app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
+			}
+			else
+			{
+				// Add Error handling middleware which catches all application specific errors and
+				// send the request to the following path or controller action.
+				//app.UseErrorHandler("/Home/Error");
+			}
+
+			// Add static files to the request pipeline.
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "Home", action = "Index" });
+
+				// Uncomment the following line to add a route for porting Web API 2 controllers.
+				// routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+			});
+		}
+	}
+}
